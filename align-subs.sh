5acbda5f9664f80dd0dad5ca933e3d7c31e7dcb3
Jean <jean.bottein@gmail.com>
2024-01-24 21:55:20 -0500

    feat: improvements and fixes

--- align-subs.sh
+++ align-subs.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+function find_video {
+  local path="$1"
+  local name="$2"
+  find "$path" -type f \( -iname "*$name*.mp4" -or -iname "*$name*.mkv" -or -iname "*$name*.avi" \) | sort | head -n 1 
+}
+
 for srt_file in "$@" ; do
     # Check if the file is an srt file
     if [[ "$srt_file" != *.srt ]] ; then
@@ -7,23 +13,31 @@ for srt_file in "$@" ; do
         continue
     fi
 
-    filename=$(basename -- "$srt_file")
-    lang=$(echo $filename | sed "s/.*\.\([A-z][A-z]\)\.srt$/\1/")
-    filename_without_lang=$(echo "$filename" | sed 's/\(\.[A-z][A-z]\)\{0,1\}\.srt//')
+    filename="$(basename -- "$srt_file")"
+    path="$(dirname -- "$srt_file")"
+    lang="$(echo $filename | sed "s/.*\.\([A-z][A-z]\)\.srt$/\1/")"
+    filename_without_lang="$(echo "$filename" | sed 's/\(\.[A-z][A-z]\)\{0,1\}\.srt//')"
 
     # Find the video file that matches the filename
-    video_file=$(find . -type f \( -iname "*$filename_without_lang*.mp4" -or -iname "*$filename_without_lang*.avi" -or -iname "*$filename_without_lang*.mkv" \) | sort | head -n 1)
+    video_file=$(find_video "$path" "$filename_without_lang")
+
+    # If no video found, let's be wild
+    if [ -z "$video_file" ] ; then
+        greedymatch=$(echo "$filename_without_lang" | grep -o -i -w -E 'S[0-9]+E[0-9]+')
+        video_file=$(find_video "$path" "$greedymatch")
+    fi
+
+    # Well, we tried enough
     if [ -z "$video_file" ] ; then
         echo "No video file found for $filename_without_lang"
         continue
     fi
 
     if [[ $lang ]] ; then
-        new_srt=${video_file%.*}.${lang}.srt
+        new_srt="${video_file%.*}.${lang}.srt"
     else
-        new_srt=${video_file%.*}.srt
+        new_srt="${video_file%.*}.srt"
     fi
-
 
     # Execute the command ffs video.mp4 -i subtitle.srt -o new_subtitle.srt
     ffs "$video_file" -i "$srt_file" -o "${srt_file%.*}.new.srt"
